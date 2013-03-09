83e721561ade9143cce9732e1196fd30a5b2e7a9
Reijo Korhonen <reijo.korhonen@gmail.com>
2013-03-10 00:46:43 +0200

    test

---
+++ Wall-E_Server.bak/Command.py
@@ -0,0 +1,93 @@
+'''
+Created on Feb 25, 2013
+
+@author: Reijo Korhonen, reijo.korhonen@gmail.com
+'''
+def enum(**enums):
+    return type('Enum', (), enums)
+
+class Command(object):
+    
+    CommandTypes = enum(Drive='D', Stop='S', Who='W', Unknown='U')
+    
+    def __init__(self, string="",
+                 number=-1, command = 'U', leftPower = 0.0, rightPower = 0.0):
+        self.number = number
+        self.command = command
+        self.leftPower = leftPower
+        self.rightPower = rightPower
+        
+        params = string.split()
+        print params
+        if len(params) >= 1:
+            try:
+                self.number = int(params[0])
+            except (ValueError):
+                self.command = Command.CommandTypes.Unknown
+                return
+                
+            print self.number
+            if len(params) >= 2:
+                command = params[1]
+                if command == Command.CommandTypes.Drive:
+                    self.command = Command.CommandTypes.Drive
+                    if len(params) >= 3:
+                        self.leftPower = float(params[2])
+                        print str(self.leftPower)
+                    if len(params) >= 4:
+                        self.rightPower = float(params[3])
+                        print str(self.rightPower)
+    
+                elif command == Command.CommandTypes.Stop:
+                    self.command = Command.CommandTypes.Stop
+                elif command == Command.CommandTypes.Who:
+                    self.command = Command.CommandTypes.Who
+                else:
+                    self.command = Command.CommandTypes.Unknown
+                print self.command
+            
+    def __str__(self):
+        if self.command == Command.CommandTypes.Drive:
+            return str(self.number) + ' ' + self.command + ' ' + str(self.leftPower) +  ' ' + str(self.rightPower)
+        elif self.command == Command.CommandTypes.Stop:
+            return str(self.number) + ' ' + self.command
+        elif self.command == Command.CommandTypes.Who:
+            return str(self.number) + ' ' + self.command
+        else:
+            return str(self.number) + ' ' + self.command
+
+    def setNumber(self, number):
+        self.number = number
+    def getNumber(self):
+        return self.number
+ 
+    def setCommand(self, command):
+        self.command = command
+    def getCommand(self):
+        return self.command
+       
+    def setLeftPower(self, leftPower):
+        self.leftPower = leftPower
+    def getLeftPower(self):
+        return self.leftPower
+        
+    def setRightPower(self, rightPower):
+        self.rightPower = rightPower
+    def getRightPower(self):
+        return self.rightPower
+ 
+        
+if __name__ == '__main__':
+    c=Command(string="12 D 0.97 0.56")
+    print "str " + str(c)
+    c=Command(string="13 S")
+    print "str " + str(c)
+    c=Command(string="13 W")
+    print "str " + str(c)
+    c=Command(string="18 oho")
+    print "str " + str(c)
+    c=Command(string="hupsis oli")
+    print "str " + str(c)
+    c=Command(number=99, command = 'D', leftPower = 0.77, rightPower = 0.55)
+    print "str " + str(c)
+    print "str(Command(str(c))) " + str(Command(str(c)))
---
+++ Wall-E_Server.bak/WalleServer.py
@@ -0,0 +1,64 @@
+'''
+Created on Feb 24, 2013
+
+@author: reijo
+'''
+
+import SocketServer
+from Command import Command
+from Romeo import Romeo
+
+HOST = '0.0.0.0'
+PORT = 2000
+
+
+class WalleRequestHandler(SocketServer.BaseRequestHandler):
+    """
+    The RequestHandler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        print "WalleRequestHandler.handle"
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        #print "{} wrote:".format(self.client_address[0])
+        print "WalleRequestHandler.handle Client wrote " + self.data
+        # just send back the same data, but upper-cased
+        print "WalleRequestHandler.handle self.request.sendall(self.data.upper())"
+        strings=self.data.split('|')
+        for string in strings:
+            print "WalleRequestHandler.handle string " + string
+            if len(string) > 0:
+                command=Command(string)
+                print command
+                command = romeo.processCommand(command)
+        self.request.sendall(str(command))
+        
+#    def handle(self):
+#        # self.rfile is a file-like object created by the handler;
+#        # we can now use e.g. readline() instead of raw recv() calls
+#        self.data = self.rfile.readline().strip()
+#        print "{} wrote:".format(self.client_address[0])
+#        print self.data
+#        # Likewise, self.wfile is a file-like object used to write back
+#        # to the client
+#        self.wfile.write(self.data.upper())
+
+
+if __name__ == "__main__":
+    romeo = Romeo()
+    #romeo.test2()
+
+    #HOST, PORT = "localhost", 2000
+
+    # Create the server, binding to localhost on port 9999
+    server = SocketServer.TCPServer((HOST, PORT), WalleRequestHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
+
