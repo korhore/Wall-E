f9c0f84a82149d201cf814af52f654944b49aef6
Reijo Korhonen <reijo.korhonen@gmail.com>
2013-08-17 00:30:21 +0300

    started making package

---
+++ Wall-E_Server/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='Wall-E_Server',
+      version='1.0',
+      py_modules=['Command','Romeo','WalleServer'],
+      packages=['Wall-E_Server'],
+      package_dir={'Wall-E_Server': '.'},
+
+      )
