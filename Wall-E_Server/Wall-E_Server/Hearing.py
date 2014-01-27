734503e8bf6b39908729a0ef09bf7e93a119be06
Reijo Korhonen <reijo.korhonen@gmail.com>
2014-01-28 00:26:59 +0200

    Hearing logic continued

--- Wall-E_Server/Wall-E_Server/Hearing.py
+++ Wall-E_Server/Wall-E_Server/Hearing.py
@@ -14,11 +14,31 @@ from Sound import Sound
 
 
 class Hearing(Thread):
+    """
+    Hears produces location information of sounds, are they coming from left or right and what is angle.
+    
+    Hearing is bases on two Ears implemented physically with microphones positioning same way than man's ears.
+    Ears produces sounds with volume and timing information.
+    
+    Theory says, that man knows location of sound from timing. Sound must go longer way to that ear that is in longer way, so 
+    if left ear is nearer, we hear sounf first in that ear. We implement this.
+    
+    Unfortunately Raspberry Pi with two microphones and threading python platform is not fast enough for this, I'm afraid.
+    Soung goes 340 m/s and if I can give 20cm difference of microphones, his means that faster sound is only
+    0.2 m / 340 m/s = 0.000588235 seconds timing difference. That time we have for several thread changes and processing.
+    This module is used to guide a robot implemented as Arduino device, so Raspberry Pi processor have another duties also.
+    
+    So we must do also something second best. We will also think that if only one microphone hears a sound,
+    sound comes on that direction. If there are two sounds in left and right ear, we will analyse level of sound
+    and get angle of sound from that power line.
+    
+    Technically we analyse sound, when we have heard it some time or it has lasted.
+    """
     
     debug = False
     log = True
     
-    SOUND_LIMIT=0.2
+    SOUND_LIMIT=0.5 # 0.2
     
     left = 'left'
     right = 'right'
@@ -35,7 +55,8 @@ class Hearing(Thread):
         
         self.queue = Queue()
         self.running = False
-        self.is_sound = False
+        #self.is_sound = False
+        self.is_sound = [False]*Hearing.LEN_EARS
        
         self.ear = [None]*Hearing.LEN_EARS
 
@@ -62,7 +83,7 @@ class Hearing(Thread):
 
         while self.running:
             sound=self.queue.get()
-            if debug:
+            if Hearing.debug:
                 print "Got sound from queue " + Hearing.ear_names[sound.get_id()]  + " State " +  sound.get_str_state() + " start_time " + str(sound.get_start_time())  + " duration " + str(sound.get_duration())  + " volume_level " + str(sound.get_volume_level())
             # TODO process which ear hears sounds first
             self.process(sound)
@@ -71,12 +92,60 @@ class Hearing(Thread):
     def process(self, sound):
         id=sound.get_id()
         self.sound[id]=sound
+       
+        if sound.get_state() == Sound.START:
+            self.is_sound[id] = True
+       # if stop or continue and sound has lasted SOUND_LIMIT
+        if sound.get_state() == Sound.STOP or (sound.get_state() == Sound.CONTINUE and sound.get_duration() > Hearing.SOUND_LIMIT):
+            other=self.other(id)
+            other_sound=self.sound[other]
+            change=False
+            
+            # if there is no sound in another ear, sound comes from this ear's direction
+            # TODO Only got Sound from left/Right  No other sound, no direction
+            if not self.is_sound[other]:
+                #print "Sound from " + Hearing.ear_names[id] + " No other sound, no direction, other sound stopped " + str( sound.get_start_time() - other_sound.get_stop_time())
+                if sound.get_start_time() - other_sound.get_stop_time() < Hearing.SOUND_LIMIT:
+                    print "Sound from " + Hearing.ear_names[id] + " No other sound, but other sound just stopped " + str( sound.get_start_time() - other_sound.get_stop_time())
+                    if other_sound.get_volume_level() > 0.0:
+                        print "Sound from " + Hearing.ear_names[id] + " direction by volume level " + str(sound.get_volume_level()/other_sound.get_volume_level())
+                    else:
+                        print "Sound from " + Hearing.ear_names[id] + " direction by volume level, but other level is zero, this direction"
+                else:  
+                    print "Sound from " + Hearing.ear_names[id] + " No other sound, no direction, other sound stopped " + str( sound.get_start_time() - other_sound.get_stop_time())
+            elif sound.get_start_time() <  other_sound.get_start_time():
+                print "Sound from " + Hearing.ear_names[id] + " This sound started before the other one, we can calculate direction"
+                if other_sound.get_start_time() - sound.get_start_time() < Hearing.SOUND_LIMIT:
+                    print "Sound from " + Hearing.ear_names[id] + " direction by timing " + str(other_sound.get_start_time() - sound.get_start_time())
+                else:
+                    if other_sound.get_volume_level() > 0.0:
+                        print "Sound from " + Hearing.ear_names[id] + " direction by volume level " + str(sound.get_volume_level()/other_sound.get_volume_level())
+                    else:
+                        print "Sound from " + Hearing.ear_names[id] + " direction by volume level, but other level is zero, this direction"
+                    
+            else:        
+                print "Sound from " + Hearing.ear_names[id] + " Other sound started after than this, we can calculate direction"
+                if sound.get_start_time() - other_sound.get_start_time() < Hearing.SOUND_LIMIT:
+                    print "Sound from " + Hearing.ear_names[other] + " direction by timing " + str(sound.get_start_time() - other_sound.get_start_time())
+                else:
+                    if sound.get_volume_level() > 0.0:
+                        print "Sound from " + Hearing.ear_names[other] + " direction by volume level " + str(other_sound.get_volume_level()/sound.get_volume_level())
+                    else:
+                        print "Sound from " + Hearing.ear_names[other] + " direction by volume level, but this level is zero, other direction"
+                    
+        if sound.get_state() == Sound.STOP:
+            self.is_sound[id] = False
+
+ 
+    def analyse(self, sound):
+        id=sound.get_id()
+        self.sound[id]=sound
         other=self.other(id)
         other_sound=self.sound[other]
         change=False
-        if not self.is_sound:
+        if not self.is_sound[id]:
             if sound.get_state() == Sound.START:
-                if debug:
+                if Hearing.debug:
                     print "process START sound from " + Hearing.ear_names[id]
                 if other_sound.get_start_time() > sound.get_start_time():
                     print "process sound other has later start time"
@@ -85,7 +154,7 @@ class Hearing(Thread):
                     else:
                         print "Too old sound, not with other, sound state keeps same"
                 else:
-                    if debug:
+                    if Hearing.debug:
                         print "process this sound was later"
                     if sound.get_start_time() - other_sound.get_stop_time() < Hearing.SOUND_LIMIT:
                         print "This sound start is close to other sound stop, continue previous sounds"
@@ -111,7 +180,6 @@ class Hearing(Thread):
                 print "SOUND STARTED ON " + Hearing.ear_names[self.sound_ear_id]
             else:
                 print "SOUND STOPPED ON " + Hearing.ear_names[self.sound_ear_id]
- 
 
     def other(self, id):
         return (id+1) % Hearing.LEN_EARS   
