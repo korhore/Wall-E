148a8bd874eae0069e6daff1709994927a9fb231
Reijo Korhonen <reijo.korhonen@gmail.com>
2014-02-10 01:33:47 +0200

    continued the idea of sensations

---
+++ WalleCapabilitiesTest/src/com/walle/capabilities/test/SensationTest.java
@@ -0,0 +1,176 @@
+package com.walle.capabilities.test;
+
+import com.walle.capabilities.Sensation;
+import com.walle.capabilities.Sensation.SensationType;
+
+import junit.framework.TestCase;
+
+public class SensationTest extends TestCase {
+
+	public SensationTest(String name) {
+		super(name);
+	}
+
+/*	
+	public SensationTest() {
+		super("com.walle.capabilities", Sensation.class);
+		}
+*/
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+	
+	public final void testSensation() {
+		Sensation s = new Sensation("12 D 0.97 0.56");
+		
+		assertTrue("number should be 12", s.getNumber() == 12);
+		assertTrue("SensationType should be Drive", s.getSensationType() == SensationType.Drive);
+		assertEquals("LeftPower should be 0.97", 0.97f, s.getLeftPower(), 0.0001f);
+		assertEquals("RightPower should be 0.56", 0.56f, s.getRightPower(), 0.0001f);
+		
+		s = new Sensation("13 S");
+		assertEquals("number should be 13", 13, s.getNumber());
+		assertTrue("SensationType should be Stop", s.getSensationType() == SensationType.Stop);
+
+		s = new Sensation("14 W");
+		assertEquals("number should be 14", 14, s.getNumber());
+		assertTrue("SensationType should be Who", s.getSensationType() == SensationType.Who);
+
+		s = new Sensation("15 H -0.75");
+		assertEquals("number should be 15", 15, s.getNumber());
+		assertTrue("SensationType should be Hear", s.getSensationType() == SensationType.Hear);
+		assertEquals("Hear should be -0.75", -0.75f, s.getHear(), 0.0001f);
+
+		s = new Sensation("16 A 0.75");
+		assertEquals("number should be 16", 16, s.getNumber());
+		assertTrue("SensationType should be Azimuth", s.getSensationType() == SensationType.Azimuth);
+		assertEquals("Azimuth should be 0.75", 0.75f, s.getAzimuth(), 0.0001f);
+
+		s = new Sensation("17 P 12300");
+		assertEquals("number should be 17", 17, s.getNumber());
+		assertTrue("SensationType should be Picture", s.getSensationType() == SensationType.Picture);
+		assertEquals("ImageSize should be 12300", 12300, s.getImageSize());
+
+		try
+		{
+			s = new Sensation("18 oho");
+		    fail("Should have thrown IllegalArgumentException");
+		 }
+		 catch(IllegalArgumentException e)
+		 {
+		    //success
+		 }
+	}
+	
+	/*
+
+	public final void testSensationIntSensationType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSensationIntSensationTypeFloatFloat() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSensationIntSensationTypeFloat() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSensationIntSensationTypeDirectionSensationTypeArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSensationString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetNumber() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetSensationType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetSnsationType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetLeftPower() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetLeftPower() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetRightPower() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetRightPower() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetHear() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetHear() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetAzimuth() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetAzimuth() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetImageSize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetImageSize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetDirection() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetDirection() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetCapabilities() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetStrCapabilities() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetCapabilities() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testToString1() {
+		fail("Not yet implemented"); // TODO
+	}
+*/
+}
